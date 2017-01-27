353c338d3333ca5d6eea6a206244abb6d963fc13
Hai Vu <haivu@Hais-MacBook-Air.local>
2017-01-26 20:35:17 -0600

    create class and main

---
+++ mileage.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+class Mpg_log{
+    double last_odometer;
+    double this_odometer;
+    double this_gas;
+    
+public:
+    void Mpg_log(double starting_odometer);
+    void buy_gas(double odometer, double gas);
+    double get_current_mpg();
+};
+
+void Mpg_log::Mpg_log(double starting_odometer)
+{
+    last_odometer = starting_odometer;
+    
+}
+
+void Mpg_log::buy_gas(double odometer, double gas)
+{
+    this_odometer = odometer;
+    this_gas = gas;
+}
+
+double Mpg_log::get_current_mpg()
+{
+    
+    double result;
+    result = (this_odometer - last_odometer) / this_gas;
+    
+    return result;
+    
+    
+}
+int main(){
+    double starting_odometer,odometer,gas;
+    cout << "Initial odometer: ";
+    cin >> starting_odometer;
+    Mpg_log mpg_log(starting_odometer);
+            cout << "Odometer: ";
+        cin >> odometer;
+        
+        cout << "Gallons: ";
+        cin >> gas;
+        
+        mpg_log.buy_gas(odometer,gas);
+        cout << "This mpg: " << mpg_log.get_current_mpg() <<'\n';
+    
+}
